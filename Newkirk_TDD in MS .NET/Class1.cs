dcc5f06ae53965a10be114d4c4ab4c1fe112c1bb
Volodymyr Tsurko <chegnenado@yahoo.com>
2020-01-04 13:09:26 +0200

    Add project files.

---
+++ Newkirk_TDD in MS .NET/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Newkirk_TDD_in_MS.NET
+{
+    public class Class1
+    {
+    }
+}
